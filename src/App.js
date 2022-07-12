954d095f047311f938701209d1d27d65e7c0cb19
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-12 14:08:01 +0900

    radio button "red" "blue" "yellow"

--- src/App.js
+++ src/App.js
@@ -2,37 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import React,{useState} from 'react';
 
-const values = [
-  {id: 1, item: "HTML"},
-  {id: 2, item: "CSS"},
-  {id: 3, item: "JavaScript"}
-];
+const InputRadio = () => {
+  const [checkedValue, setCheckedValue] = useState("赤");
+  const handleChange = (e) => setCheckedValue(e.target.value);
 
-const SelectItems = values.map((value) => {
   return(
-    <option value={value.item} key={value.id}>
-      {value.item}
-    </option>
-  );
-});
-
-const InputSelectBox = () => {
-  const [selectedValue, setSelectedValue] = useState(values[0]['item']);
-
-  const handleChange = (e) => {
-    setSelectedValue(e.target.value);
-  };
-
-  return (
-    <div>
-      <p>
-        現在選択されている値：<b>{selectedValue}</b>
-      </p>
-      <select value={selectedValue} onChange={handleChange}>
-        {SelectItems}
-      </select>
+    <div className='App'>
+      <p>現在選択されている値：<b>{checkedValue}</b></p>
+      <label>
+        <input type="radio" value="赤" onChange={handleChange} checked={checkedValue === "赤"} />赤
+      </label>
+      <label>
+        <input type="radio" value="青" onChange={handleChange} checked={checkedValue === "青"} />青
+      </label>
+      <label>
+        <input type="radio" value="黄" onChange={handleChange} checked={checkedValue === "黄"} />黄
+      </label>
     </div>
   );
 };
 
-export default InputSelectBox;
+export default InputRadio;
