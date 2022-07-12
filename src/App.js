2f8611ea31ec6b56c9757daa300df0d3db0cd222
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-12 13:52:51 +0900

    select box

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import React,{useState} from 'react';
 
-const InputText = () => {
-  const [inputTextValue, setInputTextValue] = useState("");
+const InputSelectBox = () => {
+  const [selectedValue, setSelectedValue] = useState("HTML");
 
-  const [text, setText] = useState("JavaScript");
-
-  const handleChange = (e) => setInputTextValue(e.target.value);
-
-  const handleClick = () => {
-    setText(inputTextValue);
-    setInputTextValue("");
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
   };
-  
+
   return (
-    <div className='App'>
-      <h1>I love {text}!!</h1>
-      <input type="text" value={inputTextValue} onChange={handleChange} />
-      <input type="button" value="入力" onClick={handleClick} />
+    <div>
+      <p>
+        現在選択されている値：<b>{selectedValue}</b>
+      </p>
+      <select value={selectedValue} onChange={handleChange}>
+        <option value="HTML">HTML</option>
+        <option value="CSS">CSS</option>
+        <option value="JavaScript">JavaScript</option>
+      </select>
     </div>
   );
 };
 
-export default InputText;
+export default InputSelectBox;
