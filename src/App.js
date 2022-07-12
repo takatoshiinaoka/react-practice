f8da54512c38b00ef6209d5353030f93b9c77541
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-12 13:20:36 +0900

    クリック時のイベントハンドリング

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-const numbers = [2, 4, 6, 8];
-
-const ListItems = () => {
-
-  // map() を利用して、配列 numbers から要素を1つずつ取り出して
-  // 変数に格納し li タグで囲む
-  // li タグに文字列の key属性を含める
-  // toString() は数値を文字列に変換するメソッド
-  const items = numbers.map((item) => 
-    <li key={item.toString()}>{item}</li>);
-
-  // li タグで囲った実行結果 item を ul タグで囲み、JSXに埋め込む
-  return <ul>{items}</ul>
+const handleClick = () => {
+  console.log("clickされました!");
 };
 
-export default ListItems;
+const SampleButton = () => {
+  return <input type="button" value="ボタン" onClick={handleClick} />;
+};
+
+export default SampleButton;
