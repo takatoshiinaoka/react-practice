ab24c6d05ca3055594fa1bc530dfd003441b1233
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-13 15:10:50 +0900

    SampleComponent Focus useRef

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
 // useMemo()でラップした例
-import React,{ useState, useMemo } from "react"
+import React,{ useRef } from "react"
 import "./App.css"
 
 
-const square = (params) => {
-  // [...Array(1000).keys()]は、0からn-1までの整数が順番に並んだ配列を作る
-  const testDate = [...Array(5000).keys()]
-  params && testDate.forEach(() => {
-    console.log(
-      `「計算：B + 1」がボタンクリックされ、square関数実行中、
-        ループ処理を${testDate.length}回実行中。。。`
-      )
-  })
+const SampleCompoent = () => {
+  const inputRefObject = useRef(null)
 
-  return params * params
-}
+  const handleClickFocus = () => inputRefObject.current.focus()
 
-const Counter = () => {
-  const [countStateA, setCountStateA] = useState(0)
-  const [countStateB, setCountStateB] = useState(0)
-
-  const countResultA = () => {
-    setCountStateA(c => c+1)
-    console.log("計算：A + 1ボタンがクリックされました")
-  }
-
-  const countResultB = () => {
-    setCountStateB(c => c+1)
-    console.log("計算：B + 1ボタンがクリックされました")
-  }
-
-  const squareArea = useMemo(() => square(countStateB), [countStateB])
-  // const squareArea = square(countStateB)
+  const handleClickBlur = () => inputRefObject.current.blur()
+  
 
   return (
-    <>
-      <p>
-        計算結果A：{countStateA} 
-        <button onClick={countResultA}>計算：A + 1</button>
-      </p>
-      <p>
-        計算結果B：{countStateB}  
-        <button onClick={countResultB}>計算：B + 1</button>
-      </p>
-      <p>【正方形の面積】</p>
-      <p>計算結果B × 計算結果B = {squareArea}</p>
-    </>
+    <div className="App">
+      <input ref={inputRefObject} type="text" />
+      <button onClick={handleClickFocus}>フォーカスする</button>
+      <button onClick={handleClickBlur}>フォーカスを外す</button>
+    </div>
   )
 }
 
-export default Counter
+export default SampleCompoent
