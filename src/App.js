b992995cd5d8358e298426eeb3b0b40c2a411b5c
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-13 14:25:01 +0900

    Counter useCallback()で親コンポーネントの関数をラップした例

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React,{ useState } from "react"
+import React,{ useCallback, useState } from "react"
 import "./App.css"
 
 
-const CountResult = React.memo(({ text, countState }) => {
-  countState && console.log(`${text}ボタンがクリックされました！`)
-  return(
-    <p>
-      {text}:{countState}
-    </p>
-  )
+const Button = React.memo(({counterState, buttonValue}) => {
+  console.log(`${buttonValue}がクリックされました!`)
+  return <button onClick={counterState}>{buttonValue}</button>
 })
-
 
 const Counter = () => {
   const [countStateA, setCountStateA] = useState(0)
   const [countStateB, setCountStateB] = useState(0)
   
-  const countIncrementA = () =>
-    setCountStateA(prevCountStateA => prevCountStateA + 1)
-  
-  const countIncrementB = () =>
-    setCountStateB(prevCountStateB => prevCountStateB + 1)
-  
-  const countReset = () => {
-    setCountStateA(0)
-    setCountStateB(0)
-  }
+  const countIncrementA = useCallback(() =>
+    setCountStateA(countStateA + 1), [countStateA])
+
+  const countIncrementB = useCallback(() =>
+    setCountStateB(countStateB + 1), [countStateB])
 
   return (
     <>
-      <CountResult text="Aボタン" countState={countStateA} />
-      <CountResult text="Bボタン" countState={countStateB} />
-      <button onClick={countIncrementA}>A ボタン</button>
-      <button onClick={countIncrementB}>B ボタン</button>
-      <button onClick={countReset}>リセット</button>
+      <p>A ボタン{countStateA}</p>
+      <p>B ボタン{countStateB}</p>
+      <Button counterState={countIncrementA} buttonValue="Aボタン" />
+      <Button counterState={countIncrementB} buttonValue="Bボタン" />
     </>
   )
 }
-
 
 export default Counter
