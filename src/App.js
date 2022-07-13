a569aecd45a29d6e3c42e054b2a8d9e6bf65c69f
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-13 13:31:23 +0900

    useEffect Timer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import React,{ useState,useEffect } from "react"
 import "./App.css"
 
+
 const INITIAL_COUNT = 0
 
-const SampleComponent = () => {
+
+const Timer = () => {
+
   const [count, setCount] = useState(INITIAL_COUNT)
-  
-  const callbackFunction = () => {
-    document.title = `${count}回クリックされました`
-  }
-
-  useEffect(callbackFunction, [count])
-
-  const countIncrement = () => {
+  const countReset = () => setCount(INITIAL_COUNT)
+  const countIncrement = () =>{
     setCount((prevCount) => prevCount + 1)
+    console.log("カウントアップ +1")
   }
 
-  const countReset = () => {
-    setCount(INITIAL_COUNT)
+  const callbackFunction = () =>{
+    alert("副作用関数が実行されました")
+    const timer = setInterval(countIncrement, 1000)
+    return () => {
+      console.log("timerが削除されました!")
+      clearInterval(timer)
+    }
   }
+
+  useEffect(callbackFunction, [])
 
   return (
     <div className="App">
       <p>現在のカウント数：{count}</p>
-      <button onClick={countIncrement}>+1 ボタン</button>
-      <button onClick={countReset}>リセット</button>
+      <button onClick={countReset}>RESET</button>
     </div>
   )
 }
 
-export default SampleComponent
+
+export default function App(){
+
+  const [display, toggleDisplay] = useState(false)
+  const handleToggleDisplay = () => toggleDisplay(!display)
+  return (
+    <>
+      <button onClick={handleToggleDisplay}>
+        {display ? "タイマーを非表示" : "タイマーを表示"}
+      </button>
+      {display && <Timer />}
+    </>
+  )
+}
