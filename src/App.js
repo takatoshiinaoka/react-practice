bd134683aeed8982505fe4c60485e7c589cef802
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-07-14 10:31:49 +0900

    Make Custom Hooks "useCounter"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React,{ createContext, useContext } from "react"
+import React,{ useState, createContext, useContext } from "react"
 import "./App.css"
 
-import { TextProvider } from "./TextProvider"
-import {First} from "./First"
-import { CountProvider } from "./CountProvier"
+
+const useCounter = (initialCount) => {
+  const [count, setCount] = useState(initialCount)
+
+  const countAdd = () => setCount((prevCount)=>prevCount + 1)
+  const countSub = () => setCount((prevCount)=>prevCount - 1)
+  const countReset = () => setCount(initialCount)
+
+  return {count, countAdd, countSub, countReset}
+}
 
 
-export default function App(){
-  return (
-    <div className="App">
-      <CountProvider>
-        <First />
-      </CountProvider>
+const CounterText = (props) => <p>現在のカウント数：{props.count}</p>
+
+
+const Counter = () => {
+  
+  const {count, countAdd, countSub, countReset} = useCounter(0)
+
+  return(
+    <div className="counterContainer">
+      <CounterText count={count} />
+      <button onClick={countAdd}>ボタン+1</button>
+      <button onClick={countSub}>ボタン-1</button>
+      <button onClick={countReset}>リセット</button>
     </div>
   )
 }
+
+export default Counter
